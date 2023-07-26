44ed16580250f1ba3dab5abdf8599f5bd623985e
Roman Deriga <deriga.roman@gmail.com>
2023-07-26 14:21:25 +0300

    Add progress bar

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 import ptbot
-import random
 from pytimeparse import parse
 from dotenv import dotenv_values
 
+from render_progressbar import render_progressbar
 
 CONFIG = dotenv_values('.env')
 
-def wait(chat_id, question):
-    bot.create_timer(5, reply, chat_id=chat_id, question=question)    
 
-
-def set_timer(chat_id, request):
+def set_timer(chat_id, request, bot):
     timer_secs = parse(request)
-    message_id = bot.send_message(chat_id, f'Обратный отсчет стартует')
-    bot.create_countdown(timer_secs, notify_progress, chat_id=chat_id, message_id=message_id)
-    bot.create_timer(timer_secs, timer_notify, chat_id=chat_id, message='Время вышло')
+    if timer_secs is not None:
+        message_id = bot.send_message(chat_id, f'Обратный отсчет стартует')
+        bot.create_countdown(timer_secs, timer_notify_progress, chat_id=chat_id, message_id=message_id, total_secs=timer_secs, bot=bot)
+        bot.create_timer(timer_secs, timer_notify, chat_id=chat_id, message='Время вышло', bot=bot)
+    else:
+        bot.send_message(chat_id, 'Было введено не корректное время.')
 
 
-def timer_notify(message, chat_id):
+def timer_notify(message, chat_id, bot):
     bot.send_message(chat_id, message)
 
 
-def notify_progress(secs_left, chat_id, message_id):
-    message = f'Осталось {secs_left} секунд'
+def timer_notify_progress(secs_left, chat_id, message_id, total_secs, bot):
+    progress_bar = render_progressbar(total_secs, total_secs-secs_left, length=20)
+    message = f'Осталось {secs_left} секунд\n{progress_bar}'
     bot.update_message(chat_id, message_id, message)
-    
 
 
-def reply(chat_id, question):
-    answers = ('Yes', 'No', 'Maybe')
-    answer = random.choice(answers)
-    print(f'пользователь с id {chat_id} написал {question}')
-    bot.send_message(chat_id, f'''Какого хрена ты мне это написал? - {question}
-                     Мой ответ - {answer}''')
+def main():
+    bot = ptbot.Bot(CONFIG['TELEGRAM_TOKEN'])
+    bot.reply_on_message(set_timer, bot=bot)
+    bot.run_bot()
 
 
-def echo_reply(chat_id, message):
-    bot.send_message(chat_id, message)
-
-answers = ('Yes', 'No', 'Maybe')
-answer = random.choice(answers)
-bot = ptbot.Bot(CONFIG['TELEGRAM_TOKEN'])
-#bot.send_message(CONFIG['TG_CHAT_ID'], f'Бот отвечает: {answer}')
-#bot.reply_on_message(echo_reply)
-bot.reply_on_message(set_timer)
-bot.run_bot()
+if __name__ == '__main__':
+    main()
